6ff6a2e64a8eb6e1f3e96c28749c0bfc41357c4d
Eyob <eyobmalik@gmail.com>
2026-07-19 13:41:01 +0400

    Initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,169 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+      <h1 className="text-5xl">Hello Dubai Petrol Heads</h1>
+      <br />
+      <div>
+        <div>
+          <img
+            src="https://preview.redd.it/wts-infiniti-qx70-2015-v0-lx4zq87obzdh1.jpg?width=640&crop=smart&auto=webp&s=e71548086ea7934f42e88c9209f1d3f661b68afd"
+            alt="WTS infiniti QX70 2015"
+          />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
+        <div className="text-xl">
+          <strong>WTS infiniti QX70 2015</strong>
+        </div>
+        <div>
+          <a href="https://reddit.com/r/DubaiPetrolHeads/comments/1uzu7n4/wts_infiniti_qx70_2015/">
+            Reddit Link{" "}
           </a>
         </div>
-      </main>
+        <div>
+          price: <strong>28000</strong>(non-negotiable)
+        </div>
+        <div>
+          VIN: JNRCS1MW5FM640556
+          <a href="https://www.google.com/search?q=JNRCS1MW5FM640556">google</a>
+        </div>
+        <div>Make: infiniti</div>
+        <div>Model: QX70 Luxury</div>
+        <div>
+          Region: <strong>GCC</strong>
+        </div>
+        <div>Year:2015</div>
+        <div>Mileage: 167000</div>
+        <div>Category: SUV</div>
+        <div>Time posted: Sat, 18 Jul 2026 12:09:36 +0000</div>
+        <br />
+
+        <div>
+          <img
+            src="https://preview.redd.it/wts-2018-camaro-2lt-rs-107-500km-gcc-spec-v0-ir0niyxeg5eh1.jpg?width=640&crop=smart&auto=webp&s=80a3ea6dc055fb848a61e94a7fea8891c3636890"
+            alt="[WTS] - 2018 Camaro 2LT RS - 107,500km - GCC Spec"
+          />
+        </div>
+        <div className="text-xl">
+          <strong>[WTS] - 2018 Camaro 2LT RS - 107,500km - GCC Spec</strong>
+        </div>
+        <div>
+          <a href="https://reddit.com/r/DubaiPetrolHeads/comments/1v0lbat/wts_2018_camaro_2lt_rs_107500km_gcc_spec/">
+            Reddit Link{" "}
+          </a>
+        </div>
+        <div>
+          price: <strong>59000</strong>(non-negotiable)
+        </div>
+        <div>
+          VIN: 1G1F91RS1J0180884
+          <a href="https://www.google.com/search?q=1G1F91RS1J0180884">google</a>
+        </div>
+        <div>Make: Chevrolet</div>
+        <div>Model: Camaro 2LT RS</div>
+        <div>
+          Region: <strong>GCC</strong>
+        </div>
+        <div>Year:2018</div>
+        <div>Mileage: 107500</div>
+        <div>Category: other</div>
+        <div>Time posted: Sun, 19 Jul 2026 08:46:53 +0000</div>
+        <br />
+
+        <div>
+          <img
+            src="https://preview.redd.it/wts-mercedes-benz-sl-400-v0-psftfls31udh1.jpg?width=640&crop=smart&auto=webp&s=a133d3d8a94885dded1406d4aecd5f05a87a9a22"
+            alt="WTS: Mercedes-Benz SL 400"
+          />
+        </div>
+        <div className="text-xl">
+          <strong>WTS: Mercedes-Benz SL 400</strong>
+        </div>
+        <div>
+          <a href="https://reddit.com/r/DubaiPetrolHeads/comments/1uz7rsp/wts_mercedesbenz_sl_400/">
+            Reddit Link{" "}
+          </a>
+        </div>
+        <div>
+          price: <strong>90000</strong>(non-negotiable)
+        </div>
+        <div>
+          VIN: WDDJK6FA6FF036353
+          <a href="https://www.google.com/search?q=WDDJK6FA6FF036353">google</a>
+        </div>
+        <div>Make: Mercedes-Benz</div>
+        <div>Model: SL 400</div>
+        <div>
+          Region: <strong>GCC</strong>
+        </div>
+        <div>Year:2015</div>
+        <div>Mileage: 88000</div>
+        <div>Category: Convertible</div>
+        <div>Time posted: Fri, 17 Jul 2026 18:21:24 +0000</div>
+        <br />
+
+        <div>
+          <img
+            src="https://preview.redd.it/wts-alfa-romeo-stelvio-veloce-2-0-2024-v0-m4pan2l0c5eh1.jpg?width=640&crop=smart&auto=webp&s=25da9df71dd239a98a03455f7484a1a5364f39ff"
+            alt="WTS: Alfa Romeo Stelvio Veloce 2.0 2024"
+          />
+        </div>
+        <div className="text-xl">
+          <strong>WTS: Alfa Romeo Stelvio Veloce 2.0 2024</strong>
+        </div>
+        <div>
+          <a href="https://reddit.com/r/DubaiPetrolHeads/comments/1v0kwp8/wts_alfa_romeo_stelvio_veloce_20_2024/">
+            Reddit Link{" "}
+          </a>
+        </div>
+        <div>
+          price: <strong>152000</strong>(negotiable)
+        </div>
+        <div>
+          VIN: ZASPAHDN6R7D80862
+          <a href="https://www.google.com/search?q=ZASPAHDN6R7D80862">google</a>
+        </div>
+        <div>Make: Alfa Romeo</div>
+        <div>Model: Stelvio Veloce</div>
+        <div>
+          Region: <strong>GCC</strong>
+        </div>
+        <div>Year:2024</div>
+        <div>Mileage: 39780</div>
+        <div>Category: SUV</div>
+        <div>Time posted: Sun, 19 Jul 2026 08:22:15 +0000</div>
+        <br />
+
+        <div>
+          <img
+            src="https://preview.redd.it/wts-2022-mercedes-benz-gla-200-amg-line-v0-oxcm1tswm4eh1.jpg?width=640&crop=smart&auto=webp&s=66fe252b66c5d45af3ef59a7117c274ffbdc4853"
+            alt="WTS 2022 Mercedes-Benz GLA 200 AMG Line"
+          />
+        </div>
+        <div className="text-xl">
+          <strong>WTS 2022 Mercedes-Benz GLA 200 AMG Line</strong>
+        </div>
+        <div>
+          <a href="https://reddit.com/r/DubaiPetrolHeads/comments/1v0iv3m/wts_2022_mercedesbenz_gla_200_amg_line/">
+            Reddit Link{" "}
+          </a>
+        </div>
+        <div>
+          price: <strong>176000</strong>(negotiable)
+        </div>
+        <div>
+          VIN: W1N4N8HBXNJ344873
+          <a href="https://www.google.com/search?q=W1N4N8HBXNJ344873">google</a>
+        </div>
+        <div>Make: Mercedes-Benz</div>
+        <div>Model: GLA 200 AMG Line</div>
+        <div>
+          Region: <strong>GCC</strong>
+        </div>
+        <div>Year:2022</div>
+        <div>Mileage: 58000</div>
+        <div>Category: SUV</div>
+        <div>Time posted: Sun, 19 Jul 2026 06:23:53 +0000</div>
+        <br />
+      </div>
     </div>
   );
 }
